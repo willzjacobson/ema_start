87b8526a43052e143f88828c82afca488534971b
Will Jacobson <willjacobson1@gmail.com>
2018-11-05 23:08:09 -0800

    server rendering works

--- app/components/Table.js
+++ app/components/Table.js
@@ -5,80 +5,77 @@ import ReactHtmlParser from 'react-html-parser';
 import TableCell from './TableCell';
 import * as sourceActions from '../actions/source';
 
-
 class Table extends Component {
-    handleFetchSourceData = (service, slot) => {
-        // this.props.sourceActions.fetchSource(service, slot);
-        console.log('fetchSource');
-    }
+  handleFetchSourceData = (service, slot) => {
+    // this.props.sourceActions.fetchSource(service, slot);
+    console.log('fetchSource');
+  };
 
-    render() {
-        const s = this.props.source;
-        const rows = [];
-        const firstRow = [];
-        firstRow.push(<th>service</th>)
-        const tableHeads = [ReactHtmlParser('<th>service</th>')];        
-        let firstRound = true;
-        for (let service in s) {
-            const cells = [];
-            if (typeof s[service] !== 'object') continue;
-            for (let slot in s[service]) {
-                cells.push(
-                    <TableCell 
-                        key={slot}
-                        service={service}
-                        handleFetchSourceData={this.handleFetchSourceData}
-                        slot={slot}
-                        sourceData={s[service][slot]}
-                    />)
-                if (firstRound) tableHeads.push(<th key={slot}>{slot}</th>)
-            }
-            rows.push(
-                <tr key={service}>
-                    <td>{service}</td>
-                    {cells}
-                </tr>
-            );
-            firstRound = false;
-        }
-        return(
-            <div>
-                <table className="table table-hover">
-                    <thead>
-                        <tr style={styles.titleStyle}>{tableHeads}</tr>
-                    </thead>
-                    <tbody>
-                        {rows}
-                    </tbody>
-                </table>     
-            </div>
+  render() {
+    const s = this.props.source;
+    const rows = [];
+    const firstRow = [];
+    firstRow.push(<th>service</th>);
+    const tableHeads = [ReactHtmlParser('<th>service</th>')];
+    let firstRound = true;
+    for (let service in s) {
+      const cells = [];
+      if (typeof s[service] !== 'object') continue;
+      for (let slot in s[service]) {
+        cells.push(
+          <TableCell
+            key={slot}
+            service={service}
+            handleFetchSourceData={this.handleFetchSourceData}
+            slot={slot}
+            sourceData={s[service][slot]}
+          />
         );
+        if (firstRound) tableHeads.push(<th key={slot}>{slot}</th>);
+      }
+      rows.push(
+        <tr key={service}>
+          <td>{service}</td>
+          {cells}
+        </tr>
+      );
+      firstRound = false;
     }
+    return (
+      <div>
+        <table className="table table-hover">
+          <thead>
+            <tr style={styles.titleStyle}>{tableHeads}</tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 const styles = {
-    titleStyle: {
-        // backgroundColor: '#DCDCDC',
-        // textAlign: 'center',
-        fontWeight: 'bold',
-        fontSize: '16px'
-    }
-}
+  titleStyle: {
+    // backgroundColor: '#DCDCDC',
+    // textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: '16px',
+  },
+};
 
 function mapStateToProps(state) {
-    return {
-      source: state.source
-    };
-  }
-  
-  function mapDispatchToProps(dispatch) {
-    return {
-      sourceActions: bindActionCreators(sourceActions, dispatch),
-    };
-  }
-  
-  export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(Table);
-  
+  return {
+    source: state.source,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    sourceActions: bindActionCreators(sourceActions, dispatch),
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Table);
--- app/components/TableCell.js
+++ app/components/TableCell.js
@@ -1,85 +1,99 @@
 import React, { Component } from 'react';
 
 const customStyles = {
-    content : {
-      top                   : '50%',
-      left                  : '50%',
-      right                 : '25%',
-      bottom                : '25%',
-      marginRight           : '-50%',
-      transform             : 'translate(-50%, -50%)'
-    }
+  content: {
+    top: '50%',
+    left: '50%',
+    right: '25%',
+    bottom: '25%',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
 };
 
 class TableCell extends Component {
-    state = { modalIsOpen: false };
-    componentDidMount() {
-        const { service, handleFetchSourceData, slot } = this.props;
-        handleFetchSourceData(service, slot);
+  state = { modalIsOpen: false };
+  componentDidMount() {
+    const { service, handleFetchSourceData, slot } = this.props;
+    handleFetchSourceData(service, slot);
+  }
+
+  openModal = () => {
+    this.setState({ modalIsOpen: true });
+  };
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
+  };
+
+  setButtonStyle = data => {
+    let buttonStyle = {
+      backgroundColor: '#D58A2A',
+      border: 'none',
+      color: 'white',
+      width: '100px',
+      height: '30px',
+      textAlign: 'center',
+      textDecoration: 'none',
+      display: 'inline-block',
+      fontSize: '14px',
+      borderRadius: '5px',
+    };
+
+    // Set different colors based on branches
+    if (data && data.properties) {
+      if (
+        data.properties.branch === 'master' ||
+        data.properties.branch === 'qa' ||
+        data.properties.branch === 'qa2' ||
+        data.properties.branch === 'integration'
+      ) {
+        buttonStyle.backgroundColor = '#4CAF50';
+      } else if (data.properties.repoUrl === 'VSTSRM') {
+        buttonStyle.backgroundColor = '#3340C1';
+      }
     }
 
-    openModal = () =>{
-        this.setState({modalIsOpen: true});
+    return buttonStyle;
+  };
+
+  render() {
+    const { service, sourceData, error } = this.props;
+    let display;
+    if (error) {
+      return (
+        <td>
+          <p>error</p>
+        </td>
+      );
     }
-
-    closeModal = () => {
-        this.setState({modalIsOpen: false});
+    if (sourceData === null || !sourceData || !sourceData.properties) {
+      return <td>...</td>;
     }
-
-    setButtonStyle = (data) => {
-        let buttonStyle = {
-            backgroundColor: '#D58A2A',
-            border: 'none',
-            color: 'white',
-            width: '100px',
-            height: '30px',
-            textAlign: 'center',
-            textDecoration: 'none',
-            display: 'inline-block',
-            fontSize: '14px',
-            borderRadius: '5px'
-        };
-
-        // Set different colors based on branches
-        if (data && data.properties) {
-            if (data.properties.branch === 'master' || data.properties.branch === 'qa' || data.properties.branch === 'qa2' || data.properties.branch === 'integration') {
-                buttonStyle.backgroundColor = '#4CAF50';
-            } else if (data.properties.repoUrl === 'VSTSRM') {
-                buttonStyle.backgroundColor = '#3340C1';
-            }
-        }
-
-        return buttonStyle;
+    if (!sourceData.properties.branch) {
+      display = sourceData.properties.repoUrl;
+    } else {
+      display = sourceData.properties.branch;
     }
-
-    render() {
-        const { service, sourceData, error } = this.props;
-        let display;
-        if (error) {
-            return <td><p>error</p></td>
-        }
-        if (sourceData === null || !sourceData || !sourceData.properties) {
-            return <td>...</td>
-        }
-        if (!sourceData.properties.branch) {
-            display = sourceData.properties.repoUrl;
-        } else {
-            display = sourceData.properties.branch;
-        }
-        return (
-            <td style={styles.cellStyle}>
-                <button style={this.setButtonStyle(sourceData)} onClick={this.openModal}>{display}</button>
-            </td>
-        )
-    }
+    return (
+      <td style={styles.cellStyle}>
+        <button
+          style={this.setButtonStyle(sourceData)}
+          onClick={this.openModal}
+        >
+          {display}
+        </button>
+      </td>
+    );
+  }
 }
 
 const styles = {
-    cellStyle: {
-        // height: '6px',
-        paddingTop: '1px',
-        paddingBottom: '1px'
-    }
-}
+  cellStyle: {
+    // height: '6px',
+    paddingTop: '1px',
+    paddingBottom: '1px',
+  },
+};
 
 export default TableCell;
